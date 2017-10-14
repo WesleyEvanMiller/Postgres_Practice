238d4ca6a4013bb46c5727a439bed2664a10c93b
Wesley Miller <wesmiller94@live.com>
2017-10-13 20:28:51 -0400

    Create postgresPractice

---
+++ postgresPractice/postgresPractice/postgresPractice.cpp
@@ -0,0 +1,20 @@
+// postgresPractice.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+
+#include <string>
+#include <iostream>
+
+#include "postgresPracticeHeader.h"
+
+postgresPractice::postgresPractice() {
+	cout << a << endl;
+}
+
+int main()
+{
+	postgresPractice postgresprac;
+	system("pause");
+    return 0;
+}
+
---
+++ postgresPractice/postgresPractice/postgresPracticeHeader.h
@@ -0,0 +1,23 @@
+
+//Postgres Practice Header File
+
+#ifndef POSTGRESPRACTICE_HEADER_H
+#define POSTGRESPRACTICE_HEADER_H
+
+#include <string>
+
+#include <stdlib.h>
+
+using namespace std;
+
+class postgresPractice {
+	public:
+		postgresPractice();
+
+		string a = "Hello";
+	private:
+
+	protected:
+
+};
+#endif
